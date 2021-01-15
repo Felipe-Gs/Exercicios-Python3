609051df1a661ddfe95caecd47448429198e0569
Felipe Gomes da Silva <77166107+Felipe-Gs@users.noreply.github.com>
2021-01-15 09:03:04 -0300

    adicionei dicas e modo de usar!

--- spammer.py
+++ spammer.py
@@ -18,8 +18,10 @@ for c in range(10):
     pyautogui.typewrite("PERTUBAR.......")
     pyautogui.press("enter")
 
-
-
+#modo de usar....
+#execute o programa e va imeditamente para plataforma que deseja enviar a msg e clicar no chat(seja ele, wspp, instagram, discord ou outro app de envio de mensagens!)
+#caso nao de tempo vc pode alterar o time.sleep(2) para time.sleep(10) por exmplo(para que vc possa ter masi tempo para entrar no app de mensagen
+#dica: deixe ele aberto rodando em segundo plano!
 
 
 
