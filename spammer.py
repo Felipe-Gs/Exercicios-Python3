2a1c8fbda594497e1ea0c876961da052b6d13f4f
Felipe Gomes da Silva <felipegs2905@gmail.com>
2021-01-15 08:57:36 -0300

    Create spammer.py

---
+++ spammer.py
@@ -0,0 +1,41 @@
+import pyautogui, time
+time.sleep(2)
+
+for c in range(10):
+    pyautogui.typewrite("EI......")
+    pyautogui.press("enter")
+    pyautogui.typewrite("TO AQUI PARA TE.....")
+    pyautogui.press("enter")
+    pyautogui.typewrite("PERTUBAR.......")
+    pyautogui.press("enter")
+for c in range(10):    
+    pyautogui.typewrite("KKKKKKKKK")
+    pyautogui.press("enter")
+    pyautogui.typewrite("NAO VAI FAZER NADA? KSKSKSK")
+    pyautogui.press("enter")
+    pyautogui.typewrite("TO AQUI PARA TE.....")
+    pyautogui.press("enter")
+    pyautogui.typewrite("PERTUBAR.......")
+    pyautogui.press("enter")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# pyautogui.doubleClick(x=27, y=44), abre a lixeira
+
+
+
+
+
